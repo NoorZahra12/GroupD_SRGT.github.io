d3e0564bf80a4fc5472f359ac5dd1b0a5e717aec
Noor Zahra <114682660+NoorZahra12@users.noreply.github.com>
2024-05-11 00:31:57 +0400

    backend
    i take it back, i hate bacdend coding (ToT)

---
+++ programming/basic.py
@@ -0,0 +1,243 @@
+import tkinter as tk
+from tkinter import ttk
+import json
+import requests
+
+class Items:
+    def __init__(self, itemCode, itemName, chasisNo, engineNo, group, brand, country, quantity, cost, date, p, sp):
+        self.itemCode = itemCode
+        self.itemName = itemName
+        self.chasisNo = chasisNo
+        self.engineNo = engineNo
+        self.group = group
+        self.brand = brand
+        self.country = country
+        self.quantity = quantity
+        self.cost = cost
+        self.date = date
+        self.p = p
+        self.sp = sp
+
+class Sales:
+    def __init__(self, name):
+        self.name = name
+
+def add_item():
+    item_data = [entry.get() for entry in entry_widgets]
+    if len(item_data) < 12:
+        item_data.extend(["0", "0.00"])
+    item = Items(*item_data)
+    items_list.append(item)
+    item_string = f"{item.itemName} | {item.itemCode} | {item.brand}"
+    item_listbox.insert(tk.END, item_string)
+    update_json()
+
+
+def delete_item():
+    selected_index = item_listbox.curselection()
+    if selected_index:
+        del items_list[selected_index[0]]
+        update_listbox()
+        save_to_json()
+
+def save_changes():
+    selection = item_listbox.curselection()
+    if selection:
+        index = selection[0]
+        for i, attribute in enumerate(attributes):
+            setattr(items_list[index], attribute, entry_widgets[i].get())
+        modified_item = items_list[index]
+        item_string = f"{modified_item.itemName} | {modified_item.itemCode} | {modified_item.brand}"
+        item_listbox.delete(index)
+        item_listbox.insert(index, item_string)
+        update_json()
+
+
+def fetch_exchange_rate():
+    try:
+        response = requests.get('https://api.exchangerate-api.com/v4/latest/USD')
+        data = response.json()
+        return data['rates']
+    except Exception as e:
+        print(f"Error fetching exchange rates: {e}")
+        return {}
+
+def calculate_selling_price():
+    try:
+        erates = fetch_exchange_rate()
+        cost = float(cost_entry.get())
+        profit_margin = float(profit_combobox.get())
+        
+        # Get the exchange rate for USD
+        usd_rate = erates['USD']
+        # Convert the cost to USD
+        cost_usd = cost / usd_rate
+        # Calculate the selling price in USD
+        selling_price_usd = cost_usd * (1 + profit_margin / 100)
+        # Display the selling price in USD
+        selling_price_num.config(text=f"{selling_price_usd:.2f}")
+    except ValueError:
+        selling_price_num.config(text="Invalid input")
+    except Exception as e:
+        selling_price_num.config(text=f"Error: {e}")
+
+
+
+def update_listbox():
+    item_listbox.delete(0, tk.END)
+    for item in items_list:
+        item_listbox.insert(tk.END, f"{item.itemName} | {item.itemCode} | {item.brand}")
+
+def load_from_json():
+    try:
+        with open("stock.json", "r") as file:
+            data = json.load(file)
+            for item_data in data:
+                item = Items(**item_data)
+                items_list.append(item)
+                update_listbox()
+    except FileNotFoundError:
+        pass
+
+def update_json():
+    with open('stock.json', 'w') as json_file:
+        data = []
+        for item in items_list:
+            item_data = vars(item)
+            item_data['p'] = float(item_data['p']) if item_data['p'] else 0.0
+            item_data['sp'] = float(item_data['sp']) if item_data['sp'] else 0.0
+            data.append(item_data)
+        json.dump(data, json_file)
+
+def show_selected_item(event):
+    if item_listbox.curselection():
+        index = item_listbox.curselection()[0]
+        item = items_list[index]
+        for i, attribute in enumerate(attributes):
+            entry_widgets[i].delete(0, tk.END)  # Clear previous entry
+            entry_widgets[i].insert(0, getattr(item, attribute))
+
+
+def filter_items(event=None):
+    search_query = search_entry.get().lower()
+    item_listbox.delete(0, tk.END)
+    found_items = []
+    for item in items_list:
+        if search_query in item["itemName"].lower() or \
+        search_query in item["itemCode"].lower() or \
+        search_query in item["brand"].lower():
+            found_items.append(f"{item['itemName']} | {item['itemCode']} | {item['brand']}")
+    if found_items:
+        for found_item in found_items:
+            item_listbox.insert(tk.END, found_item)
+    else:
+        item_listbox.insert(tk.END, "No matching results found.")
+
+root = tk.Tk()
+root.title("Item Management System")
+root.geometry("750x400")
+
+# Notebook structure
+notebook = ttk.Notebook(root)
+notebook.pack(fill="both", expand=True)
+
+item_tab = ttk.Frame(notebook)
+notebook.add(item_tab, text="Items")
+
+# Left frame
+left_frame = ttk.Frame(item_tab, padding="10")
+left_frame.grid(row=0, column=0, sticky="nsew")
+left_frame.rowconfigure(1, weight=1)
+
+search_frame = ttk.Frame(left_frame)
+search_frame.grid(row=0, column=0, sticky="nsew")
+
+search_label = ttk.Label(search_frame, text="Search:")
+search_label.grid(row=0, column=0, padx=5, pady=5)
+
+search_entry = ttk.Entry(search_frame)
+search_entry.grid(row=0, column=1, padx=5, pady=5)
+
+search_btn = ttk.Button(search_frame, text="search")
+search_btn.grid(row=0,column=2)
+
+item_listbox = tk.Listbox(left_frame, borderwidth=1, relief="sunken")
+item_listbox.grid(row=1, column=0, sticky="nsew")
+item_listbox.bind("<<ListboxSelect>>", show_selected_item)
+
+scrollbar = ttk.Scrollbar(left_frame, orient="vertical", command=item_listbox.yview)
+scrollbar.grid(row=1, column=1, sticky="ns")
+item_listbox.config(yscrollcommand=scrollbar.set)
+
+# Load items from JSON file
+with open("stock.json", "r") as file:
+    items_list = json.load(file)
+
+# Populate listbox with items from JSON file
+for item in items_list:
+    item_listbox.insert(tk.END, f"{item['itemName']} | {item['itemCode']} | {item['brand']}")
+
+
+# Right frame
+right_frame = ttk.Frame(item_tab, padding="10")
+right_frame.grid(row=0, column=1, sticky="nsew")
+
+attributes = ["itemCode", "itemName", "chasisNo", "engineNo", "group", "brand", "country", "quantity", "cost", "date"]
+entry_widgets = []
+
+for i, attribute in enumerate(attributes):
+    ttk.Label(right_frame, text=attribute.capitalize() + ":").grid(row=i, column=0, sticky="e", padx=5, pady=2)
+    entry = ttk.Entry(right_frame, width=30)
+    entry.grid(row=i, column=1, padx=5, pady=2)
+    entry_widgets.append(entry)
+
+cost_entry = entry_widgets[8]
+
+profit_label = ttk.Label(right_frame, text="Profit Margin (%):")
+profit_label.grid(row=len(attributes), column=0, padx=5, pady=2)
+
+profit_combobox = ttk.Combobox(right_frame, values=list(range(1, 101)))
+profit_combobox.grid(row=len(attributes), column=1, padx=5, pady=2)
+
+calculate_button = ttk.Button(right_frame, text="Calculate", command=calculate_selling_price)
+calculate_button.grid(row=len(attributes), column=2, padx=5, pady=2)
+
+selling_price_label = ttk.Label(right_frame, text="Selling Price:")
+selling_price_label.grid(row=len(attributes)+1, column=0, padx=5, pady=2)
+
+selling_price_num = ttk.Label(right_frame, text="0.00")
+selling_price_num.grid(row=len(attributes)+1, column=1, padx=5, pady=2)
+
+
+# 3 buttons
+add_button = ttk.Button(right_frame, text="Add", command=add_item)
+add_button.grid(row=len(attributes)+2, column=0, padx=5, pady=5)
+
+delete_button = ttk.Button(right_frame, text="Delete", command=delete_item)
+delete_button.grid(row=len(attributes)+2, column=1, padx=5, pady=5)
+
+save_button = ttk.Button(right_frame, text="Save Changes", command=save_changes)
+save_button.grid(row=len(attributes)+2, column=2, padx=5, pady=5)
+
+# Sales tab for managing sales made
+sales_tab = ttk.Frame(notebook)
+notebook.add(sales_tab, text="Sales")
+
+# Calculate tab
+calc_tab = ttk.Frame(notebook)
+notebook.add(calc_tab, text="Calculate")
+
+# Recycle Bin tab
+bin_tab = ttk.Frame(notebook)
+notebook.add(bin_tab, text="Recycle Bin")
+
+# Analysis tab
+analysis_tab = ttk.Frame(notebook)
+notebook.add(analysis_tab, text="Analysis")
+
+
+# Initialize items list
+items_list = []
+load_from_json()
+
+root.mainloop()
--- programming/stock copy.py
+++ programming/stock copy.py
@@ -4,7 +4,7 @@ import json
 import requests
 
 class Items:
-    def __init__(self, itemCode, itemName, chasisNo, engineNo, group, brand, country, quantity, cost, date):
+    def __init__(self, itemCode, itemName, chasisNo, engineNo, group, brand, country, quantity, cost, date, p="default_p_value", sp="default_sp_value"):
         self.itemCode = itemCode
         self.itemName = itemName
         self.chasisNo = chasisNo
@@ -15,6 +15,32 @@ class Items:
         self.quantity = quantity
         self.cost = cost
         self.date = date
+        self.p = p
+        self.sp = sp
+
+def fetch_exchange_rate():
+    try:
+        response = requests.get('https://api.exchangerate-api.com/v4/latest/USD')
+        data = response.json()
+        return data['rates']
+    except Exception as e:
+        print(f"Error fetching exchange rates: {e}")
+        return {}
+
+def calculate_selling_price(profit_margin_combobox):
+    try:
+        # Get the index of the last added item
+        last_index = len(items_list) - 1
+        cost = float(entry_widgets[attributes.index("cost")].get())
+        profit_margin = float(profit_margin_combobox.get())  # Get profit margin from Combobox
+        selling_price = cost * (1 + profit_margin / 100)  # Calculate selling price
+        
+        # Update the selling price label for the last added item
+        selling_price_labels[last_index].config(text=f"Selling price: {selling_price:.2f}")
+        
+    except (ValueError, IndexError):
+        selling_price_label.config(text="Invalid input or no item added")
+
 
 def add_item():
     item_data = [entry.get() for entry in entry_widgets]
@@ -22,8 +48,12 @@ def add_item():
     items_list.append(item)
     item_string = f"{item.itemName} | {item.itemCode} | {item.brand}"
     item_listbox.insert(tk.END, item_string)
-    add_button.config(text="Added", state=tk.DISABLED)
-    root.after(1000, lambda: reset_button(add_button, "Add Item"))
+    
+    # Create a new selling price label for the new item and store it in the dictionary
+    selling_price_label = ttk.Label(add_tab, text="Selling price: 0.00")
+    selling_price_label.grid(row=len(attributes) + len(items_list), columnspan=3, pady=5)
+    selling_price_labels[len(items_list) - 1] = selling_price_label
+    
     update_json()
 
 def delete_item():
@@ -31,8 +61,6 @@ def delete_item():
         index = item_listbox.curselection()[0]
         del items_list[index]
         item_listbox.delete(index)
-        delete_button.config(text="Deleted", state=tk.DISABLED)
-        root.after(1000, lambda: reset_button(delete_button, "Delete Item"))
         update_json()
 
 def save_changes():
@@ -45,31 +73,7 @@ def save_changes():
         item_string = f"{modified_item.itemName} | {modified_item.itemCode} | {modified_item.brand}"
         item_listbox.delete(index)
         item_listbox.insert(index, item_string)
-        save_button.config(text="Saved", state=tk.DISABLED)
-        root.after(1000, lambda: reset_button(save_button, "Save Changes"))
         update_json()
-
-def reset_button(button, text):
-    button.config(text=text, state=tk.NORMAL)
-
-def calculate_selling_price():
-    try:
-        cost_price = float(entry_widgets[-2].get())
-        profit = 0.5 * cost_price
-        response = requests.get('https://api.exchangerate-api.com/v4/latest/USD')
-        print("Response status code:", response.status_code)
-        data = response.json()
-        print("Exchange rate data:", data)
-        exchange_rate = data['rates'].get('AED')
-        print("Exchange rate for AED:", exchange_rate)
-        if exchange_rate:
-            selling_price = (cost_price + profit) * exchange_rate
-            selling_price_var.set(f"AED {selling_price:.2f}")
-        else:
-            selling_price_var.set("Exchange rate not available for AED")
-    except Exception as e:
-        print("Error:", e)
-        selling_price_var.set("Error calculating selling price")
 
 def show_selected_item(event):
     if item_listbox.curselection():
@@ -84,72 +88,72 @@ def update_json():
         json.dump([vars(item) for item in items_list], json_file)
 
 def load_json():
-    with open('stock.json', 'r') as json_file:
-        data = json.load(json_file)
-        for item_data in data:
-            item = Items(**item_data)
-            items_list.append(item)
-            item_string = f"{item.itemName} | {item.itemCode} | {item.brand}"
-            item_listbox.insert(tk.END, item_string)
+    try:
+        with open('stock.json', 'r') as json_file:
+            data = json.load(json_file)
+            for item_data in data:
+                item = Items(**item_data)
+                items_list.append(item)
+                item_string = f"{item.itemName} | {item.itemCode} | {item.brand}"
+                item_listbox.insert(tk.END, item_string)
+    except FileNotFoundError:
+        pass
 
 root = tk.Tk()
 root.title("Item Management System")
-root.geometry("650x400")
+root.geometry("650x450")
 
-# left frame for list
 left_frame = ttk.Frame(root, padding="10")
 left_frame.grid(row=0, column=0, columnspan=2, sticky="nsew")
-
-# Adjusting column weights to allocate more space to the left frame
 root.grid_columnconfigure(0, weight=1)
 root.grid_columnconfigure(1, weight=1)
 
-# right frame for notebook
 right_frame = ttk.Frame(root, padding="10")
 right_frame.grid(row=0, column=2, sticky="nsew")
 
-
-# listbox inside the left frame
 item_listbox = tk.Listbox(left_frame, borderwidth=1, relief="sunken")
 item_listbox.pack(fill="both", expand=True)
 item_listbox.bind("<<ListboxSelect>>", show_selected_item)
 
-# notebook inside the right frame
 notebook = ttk.Notebook(right_frame)
 notebook.pack(fill="both", expand=True)
 
-# tab for managing items
 add_tab = ttk.Frame(notebook)
 notebook.add(add_tab, text="Item")
 
-# labels and entry widgets for item details in the tab
 attributes = ["itemCode", "itemName", "chasisNo", "engineNo", "group", "brand", "country", "quantity", "cost", "date"]
 entry_widgets = []
+
 for i, attribute in enumerate(attributes):
     ttk.Label(add_tab, text=attribute.capitalize()+":").grid(row=i, column=0, sticky="e", pady=2)
     entry = ttk.Entry(add_tab, width=30)
     entry.grid(row=i, column=1, padx=5, pady=2)
     entry_widgets.append(entry)
 
-# Adding a StringVar for selling price
-selling_price_var = tk.StringVar()
-selling_price_label = ttk.Label(add_tab, textvariable=selling_price_var)
-selling_price_label.grid(row=len(attributes), columnspan=2, pady=2)
+profit_label = ttk.Label(add_tab, text="Profit Margin (%):")
+profit_label.grid(row=len(attributes), column=0, pady=2)
 
-# Bind the calculation function to the cost entry widget
-entry_widgets[-2].bind("<FocusOut>", lambda event: calculate_selling_price())
+profit_margin = ttk.Combobox(add_tab, values=list(range(1, 101)))
+profit_margin.current(49)  # Set default value to 50
+profit_margin.grid(row=len(attributes), column=1, padx=5, pady=2)
 
-# 3 buttons for add, clear, and delete
+calc_button = ttk.Button(add_tab, text="Calculate", command=lambda: calculate_selling_price(profit_margin))
+calc_button.grid(row=len(attributes), column=2, pady=2)
+
+selling_price_label = ttk.Label(add_tab, text="Selling price: 0.00")
+selling_price_label.grid(row=len(attributes)+2, columnspan=3, pady=10)
+
+# 3 buttons for add, delete, and save changes
 add_button = ttk.Button(add_tab, text="Add Item", command=add_item)
-add_button.grid(row=len(attributes)+1, column=0, pady=10, padx=5)
+add_button.grid(row=len(attributes)+3, column=0, pady=10, padx=5)
 
 delete_button = ttk.Button(add_tab, text="Delete Item", command=delete_item)
-delete_button.grid(row=len(attributes)+1, column=1)
+delete_button.grid(row=len(attributes)+3, column=1)
 
 save_button = ttk.Button(add_tab, text="Save Changes", command=save_changes)
-save_button.grid(row=len(attributes)+1, column=2, pady=10, padx=5)
+save_button.grid(row=len(attributes)+3, column=2, pady=10, padx=5)
 
 items_list = []
-load_json()  # Loading data from JSON file
+load_json()
 
 root.mainloop()
